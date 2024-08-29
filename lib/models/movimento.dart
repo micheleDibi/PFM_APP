5e158e77afc69e4f44b50d7f39319cecfb6239eb
micheledibi <micheledibi2002@gmail.com>
2024-08-29 12:17:26 +0200

    Aggiunta classe 'Movimento'

---
+++ lib/models/movimento.dart
@@ -0,0 +1,9 @@
+class Movimento {
+
+  Movimento({required this.title, required this.amount, required this.date, required this.category});
+
+  String title;
+  double amount;
+  DateTime date;
+  String category;
+}
