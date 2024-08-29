6dc1f4d05190a8bdeb479f2ea21577f278d7f0b1
micheledibi <micheledibi2002@gmail.com>
2024-08-29 19:06:14 +0200

    Aggiunto supporto per l'utilizzo dei provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:pfm_app/home.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const ProviderScope(child: App(),));
 }
 
 class App extends StatelessWidget {
