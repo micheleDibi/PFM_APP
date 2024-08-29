e2163ebcefd56154a7adc1a936c07cc8d31915fb
micheledibi <micheledibi2002@gmail.com>
2024-08-29 12:17:04 +0200

    Rimossa classe temporanea 'Item'

--- lib/models/item.dart
+++
@@ -1,9 +0,0 @@
-class Item {
-
-  Item({required this.title, required this.amount, required this.date, required this.category});
-
-  String title;
-  double amount;
-  DateTime date;
-  String category;
-}
