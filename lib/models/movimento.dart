41bfd365dfa94d8bef44686b5f6d956d08ee5ff9
micheledibi <micheledibi2002@gmail.com>
2024-08-29 16:18:22 +0200

    Aggiunto elenco di categorie per ciascun movimento finanziario

--- lib/models/movimento.dart
+++ lib/models/movimento.dart
@@ -1,9 +1,14 @@
+import 'package:pfm_app/models/categoria.dart';
+
 class Movimento {
 
-  Movimento({required this.title, required this.amount, required this.date, required this.category});
+  Movimento({required this.title, required this.amount, required this.date, required this.categorie, this.note = "", required this.tipo});
 
   String title;
   double amount;
+  String note;
   DateTime date;
-  String category;
+  String tipo;
+  List<Categoria> categorie;
+
 }
